ea0ba65e8eb2c4ccb24273e3a3dcdd62acf8ed8e
Seth Bowman <brandonsethbowman@gmail.com>
2023-12-12 14:13:56 -0600

    Add project files.

---
+++ SyntaxSugarDemoCSharp-43/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SyntaxSugarDemoCSharp_43
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Explicit typing
+            int number = 10;
+            string name = "test";
+            int num;
+
+            //Inferred typing
+            var thing = true;
+            var otherNum = 12.0m;
+
+            //Inline if/ Ternary
+
+            var age = 10;
+
+            Console.WriteLine(age >= 18 ? "You're an adult" : age < 5 ? "You're a baby" : "You're a minor");
+
+            //String interpolation vs Concatenation
+
+            var middleName = 'S';
+
+            Console.WriteLine($"Brandon {middleName} Bowman");
+            Console.WriteLine("Brandon" + " " + middleName + " " + "Bowman");
+
+
+        }
+    }
+}
